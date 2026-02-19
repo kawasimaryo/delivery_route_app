b573557d09701c5024737cf138237a0df2c66f44
RyoKawasima <ryokawasima@RyoKawasimanoMacBook-Air.local>
2026-02-19 21:52:39 +0900

    ssl_fix.rbをdevelopment環境のみに限定

--- config/initializers/ssl_fix.rb
+++ config/initializers/ssl_fix.rb
@@ -1,2 +1,7 @@
-require 'openssl'
-OpenSSL::SSL::SSLContext::DEFAULT_PARAMS[:ca_file] = '/opt/homebrew/etc/openssl@3/cert.pem'
+# frozen_string_literal: true
+
+# MacのOpenSSL証明書パス問題の修正（ローカル開発環境のみ）
+if Rails.env.development?
+  require 'openssl'
+  OpenSSL::SSL::SSLContext::DEFAULT_PARAMS[:ca_file] = '/opt/homebrew/etc/openssl@3/cert.pem'
+end
